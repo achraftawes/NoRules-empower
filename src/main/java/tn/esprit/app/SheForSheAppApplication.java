a0075b32707107864707c0ee9a4598579246f595
AmeniHamdi <44673905+AmeniHamdi@users.noreply.github.com>
2022-02-19 13:11:49 +0100

    First Commit

---
+++ src/main/java/tn/esprit/app/SheForSheAppApplication.java
@@ -0,0 +1,13 @@
+package tn.esprit.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SheForSheAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SheForSheAppApplication.class, args);
+	}
+
+}
