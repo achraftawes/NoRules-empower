a88b867e60db746f91b18628ee4c0b72b8571bab
achraftawes1 <achraf.tawes@esprit.tn>
2022-02-24 19:55:49 +0000

    Entités Posts

---
+++ src/main/java/tn/esprit/app/entities/Advertising.java
@@ -0,0 +1,45 @@
+package tn.esprit.app.entities;
+
+import java.io.Serializable;
+import java.util.Date;
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Advertising implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // 
+	@Column(name="idAdvertising")
+	Long idAdvertising;
+	String namePublication;
+	String canal;
+	Date startDate;
+	Date endDate;
+	int views;
+	int Price;
+	String Type;
+	
+	
+}
